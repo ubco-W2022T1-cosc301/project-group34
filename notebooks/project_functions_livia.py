409b4f376976c4a09237ba5ee26a487d7ee590f8
liviaz09 <77290008+liviaz09@users.noreply.github.com>
2022-12-03 17:06:52 -0800

    Livia committed changes

--- notebooks/project_functions_livia.py
+++ notebooks/project_functions_livia.py
@@ -1,21 +1,60 @@
 ## Method Chaining
 import pandas
 
-def load_and_process(url):
+def load_and_process_df1(url):
     
-    df = pandas.read_csv(url)
+    df1 = pd.read_csv(url, sep=',')
     
-    df1 = {
-        df.copy().drop(['Code'], axis=1)
-    }
+    indexEnt = df1_cleaned.loc[lambda row : row['Entity']=='World'].index
     
-    df2 = {
+    df1_1 = {
         df1
-        .rename(columns={'Share of fish stocks within biologically sustainable levels (FAO, 2020)': 'Biologically sustainable fish stocks', 'Share of fish stocks that are overexploited': 'Overexploited fish stocks'})
-        .rename(columns={'Capture fisheries production (metric tons)': 'Capture fisheries production'})
-        .rename(columns={'Fish, Seafood- Food supply quantity (kg/capita/yr) (FAO, 2020)' : 'Fish, Seafood- Food supply quantity'})
-        .rename(columns={'Commodity Balances - Livestock and Fish Primary Equivalent - Freshwater Fish - 2761 - Production - 5510 - tonnes' : 'Livestock and Fish Primary Equivalent - Freshwater Fish'})
-    }
-    
-    return df1
+        .dropna(axis=0)
 
+    }
+    df1_2 = {
+        df1_1
+        .copy().drop(['Code'], axis=1)
+
+    }
+    df1_3 = {
+        df1_2
+        .copy().drop(indexEnt, inplace=True)
+        .rename(columns={'Entity': 'Country', 'Capture fisheries production (metric tons)': 'Capture fisheries production'})
+
+    }
+    df1_4 = {
+        df1_3
+        .sort_values(by=['Capture fisheries production'], ascending = False)
+
+    }
+    return df1_4
+
+def load_and_process_df2(url):
+    
+    df2 = pd.read_csv(url, sep=',')
+    
+    indexEnt = df2_cleaned.loc[lambda row : row['Entity']=='World'].index
+    
+    df2_1 = {
+        df2
+        .dropna(axis=0)
+
+    }
+    df2_2 = {
+        df2_1
+        .copy().drop(['Code'], axis=1)
+
+    }
+    df2_3 = {
+        df2_2
+        .copy().drop(indexEnt, inplace=True)
+        .rename(columns={'Entity': 'Country', 'Fish, Seafood- Food supply quantity (kg/capita/yr) (FAO, 2020)': 'Seafood supply'})
+        
+    }
+    df2_4 = {
+        df2_3
+        .sort_values(by=['Seafood supply'], ascending = False)
+
+    }
+    return df1_4
